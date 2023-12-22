b96ea4b634fca9595f940c86133d6ccb5a172a55
Om Bhatt <omhbhatt2409@gmail.com>
2023-12-22 14:51:55 +0530

    refactor in exercise 6 done

--- Exercise/ex6.js
+++ Exercise/ex6.js
@@ -4,84 +4,87 @@ const obj2 = { hair: "long", beard: false };
 
 // > todo:  do it with Object.keys and Object.values
 const compareTwoArrays = (arr1, arr2) =>
+  Array.isArray(arr1) &&
+  Array.isArray(arr2) &&
   arr1.length === arr2.length &&
   Array.from(arr1).every((val, idx) => val === arr2[idx]);
 
 // console.log(compareTwoArrays([1, 2], [1, 2]));
 const compareTwoObjects = (obj1, obj2) => {
-  // ? approach 1
-  // return JSON.stringify(obj1) === JSON.stringify(obj2);
+  if (obj1 === null && obj2 === null) return true;
   return (
+    obj1 !== null &&
+    obj2 !== null &&
     compareTwoArrays(Object.keys(obj1), Object.keys(obj2)) &&
     compareTwoArrays(Object.values(obj1), Object.values(obj2))
   );
 };
-console.log(compareTwoArrays(obj1, obj2));
+// console.log(compareTwoObjects(null, null));
+// console.log(compareTwoObjects({}, {}));
 // * Write a program to convert string to a 2D array of objects. The first row of the string is used as the title.
 // ? inputString = "col1,col2\na,b\nc,d"
 // ? output [{'col1': 'a', 'col2': 'b'}, {'col1': 'c', 'col2': 'd'}]
+const inputString = "col1,col2\na,b\nc,d";
 const stringTo2DArrayOfObject = (str) => {
   const rows = str.split("\n");
   const keys = rows.splice(0, 1)[0].split(",");
-  let result = [];
-
-  // todo : try with single loop
-  rows.forEach((row) => {
-    const values = row.split(",");
-    let entries = [];
-    for (let index = 0; index < keys.length; index++) {
-      entries.push([keys[index], values[index]]);
-    }
-    result.push(Object.fromEntries(entries));
-  });
-  return result;
+  return rows.reduce((acc, curr) => {
+    let values = curr.split(",");
+    return acc.concat(
+      values.reduce((obj, value, idx) => {
+        obj[keys[idx]] = value;
+        return obj;
+      }, {})
+    );
+  }, []);
 };
-
+// console.log(stringTo2DArrayOfObject(inputString));
 // * Write a program to convert an array of objects to a string. That contains dynamic columns and de limiter specified.
 // ?  input = [{col1:'a', col2:'b'}, {'col1': 'c', 'col2': 'd','col3':'e'}]
 // ? output: 'Col1,col2\na,b'
-// const input1 = [
-//   { col1: "a", col2: "b" },
-//   { col1: "c", col2: "d", col3: "e" },
-// ];
+const input1 = [
+  { col1: "a", col2: "b" },
+  { col1: "c", col2: "d", col3: "e" },
+];
 const TwoDArrayOfObjectToString = (arrayOfObj) => {
-  let ans = "";
-  //  todo do with set
   const keysSet = new Set();
-  arrayOfObj.forEach((obj) => {
-    Object.keys(obj).forEach((key) => keysSet.add(key));
+  arrayOfObj.map((obj) => {
+    Object.keys(obj).map((key) => keysSet.add(key));
   });
-  arrayOfObj.forEach((obj, idx) => {
-    if (idx === 0) {
-      ans += Array.from(keysSet).join(",");
+  return arrayOfObj
+    .reduce((ans, obj, idx) => {
+      if (idx === 0) {
+        ans += Array.from(keysSet).join(",");
+        ans += "\n";
+      }
+      ans += Object.values(obj).join(",");
       ans += "\n";
-    }
-    ans += Object.values(obj).join(",");
-    ans += "\n";
-  });
-
-  return ans.slice(0, -1);
-};
-// console.log(TwoDArrayOfObjectToString(input1));
-// * Write a program to replace the names of multiple object keys.
-// ? output { firstName:'JJ', Role:'Programmer', age:22 }
-// const obj = { name: "JJ", job: "Programmer", age: 22 };
-// const renameKey = { name: "firstName", job: "Role" };
-const replaceNamesOfKeys = (obj, renameKey) => {
-  // > todo : mutate original object
-  // ? approach 1
-  // const entries = Object.entries(obj);
-  // const keysToBeChanged = Object.keys(renameKey);
-  // entries.forEach((entry) => {
-  //   if (keysToBeChanged.includes(entry[0])) {
-  //     entry[0] = renameKey[entry[0]];
-  //   }
-  // });
-  // return Object.fromEntries(entries);
+      return ans;
+    }, "")
+    .slice(0, -1);
 
   // ? approach 2
+  // let ans = "";
+  // arrayOfObj.map((obj, idx) => {
+  //   if (idx === 0) {
+  //     ans += Array.from(keysSet).join(",");
+  //     ans += "\n";
+  //   }
+  //   ans += Object.values(obj).join(",");
+  //   ans += "\n";
+  // });
+
+  // return ans.slice(0, -1);
+};
+// console.log(TwoDArrayOfObjectToString(input1));
+
+// * Write a program to replace the names of multiple object keys.
+// ? output { firstName:'JJ', Role:'Programmer', age:22 }
+const obj = { name: "JJ", job: "Programmer", age: 22 };
+const renameKey = { name: "firstName", job: "Role" };
+const replaceNamesOfKeys = (obj, renameKey) => {
   const keysToBeChanged = Object.keys(renameKey);
-  keysToBeChanged.forEach((keyToChange) => {
+  keysToBeChanged.map((keyToChange) => {
     obj[renameKey[keyToChange]] = obj[keyToChange];
     delete obj[keyToChange];
   });
@@ -93,50 +96,43 @@ const input = [1, 2, 2, 3, 4, 4, 5];
 // > todo take one output array
 
 const uniqueValues = (arr) => {
-  let output = [];
   // ? approach 1
-  // let mp = new Map();
-  // arr.forEach((el) => {
-  //   if (mp.has(el)) mp.set(el, mp.get(el) + 1);
-  //   else mp.set(el, 1);
-  // });
-  // let ans = [];
-  // mp.forEach((val, key, mp) => {
-  //   if (val === 1) ans.push(key);
-  // });
-  // ? approach 2
-  output.push(arr[0]);
-  for (let index = 1; index < arr.length; index++) {
-    const element = arr[index];
-    let foundIdx = output.findIndex((el) => el === element);
-    if (foundIdx !== -1) output.splice(foundIdx, 1);
-    else output.push(element);
-  }
-  return output;
+  let freq = {};
+  arr.map((el) => {
+    if (freq[el]) freq[el]++;
+    else freq[el] = 1;
+  });
+  return arr.filter((el) => freq[el] === 1);
 };
+
 const nonUniqueValues = (input) => {
   let temp = input[0];
-  let output = [];
-  for (let index = 1; index < input.length; index++) {
-    const element = input[index];
-    if (element === temp) {
-      output.push(element);
+  return input.slice(1).filter((el, idx) => {
+    if (el === temp) {
+      return true;
     } else {
-      temp = element;
+      temp = input[idx + 1];
+      return false;
     }
-  }
-  return output;
+  });
 };
+console.log(nonUniqueValues([1, 2, 2, 3, 4, 4, 5]));
+
 // * Write a program to remove the key-value pairs to the given keys.
 // ? output {"a":1,"c":3}
-// const input = { a: 1, b: "2", c: 3 };
-// const removeKey = ["b"];
+const input3 = { a: 1, b: "2", c: 3 };
+const removeKey = ["b"];
 
 const removeKeyValuePairs = (input, removeKey) => {
-  removeKey.forEach((el) => delete input[el]);
-  return input;
+  const keys = Object.keys(input);
+  return keys
+    .filter((key) => !removeKey.includes(key))
+    .reduce((newObj, key) => {
+      newObj[key] = input[key];
+      return newObj;
+    }, {});
 };
-
+// console.log(removeKeyValuePairs(input3, removeKey));
 // * Write a program to convert time 24 hours to 12 hours.
 // input1 = 0 ;  12am
 // input2 = 13 ; 1pm
@@ -196,14 +192,14 @@ const sortArrayOfObjects = (inputArray, orderedBy, orders) => {
   return inputArray;
 };
 
-// const input = [
-//   { name: "fred", age: 48 },
-//   { name: "barney", age: 36 },
-//   { name: "fred", age: 40 },
-// ];
-// const ordered_by = ["name", "age"];
-// const orders = ["asc", "desc"];
-// console.log(sortArrayOfObjects(input, ordered_by, orders));
+const input2 = [
+  { name: "fred", age: 48 },
+  { name: "barney", age: 36 },
+  { name: "fred", age: 40 },
+];
+const ordered_by = ["name", "age"];
+const orders = ["asc", "desc"];
+// console.log(sortArrayOfObjects(input2, ordered_by, orders));
 
 // * Write a program to combine the numbers of a given array into an array containing all combinations. ( reduce )
 
@@ -216,13 +212,4 @@ const allCombinations = (inputArray) => {
     [[]]
   );
 };
-
-const allCombinationsShortHand = (inputArray) =>
-  inputArray.reduce(
-    (combinations, number) =>
-      combinations.concat(
-        combinations.map((combination) => [number].concat(combination))
-      ),
-    [[]]
-  );
 // console.log("ans", allCombinations([1, 2]));
